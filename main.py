82b34c277a28a477fa78dfe9d713ea2ce08b5465
theawesomew <theawesomew@gmail.com>
2021-12-04 15:10:30 +1100

    Added Flask-based UI

--- main.py
+++ main.py
@@ -2,20 +2,32 @@ from utils.robot import *
 from utils.mapping import *
 from utils.pathfinding import *
 import time
+from flask import Flask, request
+
+app = Flask(__name__);
 
 robot = Robot(0, 0, 90)
 
-MAP_STRING = ""
+@app.route('/')
+def main ():
+	return '<!DOCTYPE html><html><head><meta charset="UTF-8"/><title></title><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script><script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script></head><body><div class="container-fluid"><div class="row"><a href="/drive">Drive To</a></div><div class="row"><a href="/door">Door Activation</a></div><div class="row"><a href="/video">Video Library</a></div></div></body></html>'
 
-MAP_MATRIX = parseMapData(MAP_STRING)
+@app.route('/drive')
+def drive ():
+	try:
+		robot.follow("FFFF")
+		robot.stop()
+	except KeyboardInterrupt:
+		robot.stop()
+		print("Program terminated")
 
-placeRobot(MAP_MATRIX, robot.x, robot.y)
+@app.route('/drive/<int:room>')
+def driveToRoom (room):
+	return ''
 
-placeTarget(MAP_MATRIX, 0, 0)
+@app.route('/video')
+def videoLibrary ():
+	return ''
 
-try:
-    robot.follow("R")
-    robot.stop()
-except KeyboardInterrupt:
-    robot.stop()
-    print("Program terminated")
+if __name__ == "__main__":
+    app.run(debug=True)
