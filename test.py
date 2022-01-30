32238d8958264badf579852c979c15b9508c48b9
theawesomew <theawesomew@gmail.com>
2022-01-30 14:17:30 +1100

    Add files via upload

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import time
 robot = Robot(0,0,90)
 
 try:
-    secondsPerDegree = (1/(2*degrees(asin(9/18.25))))*2.25
+    secondsPerDegree = (1/(2*degrees(asin(9/18.25))))
     robot.turn(True)
     time.sleep(secondsPerDegree * 90)
     robot.stop()
