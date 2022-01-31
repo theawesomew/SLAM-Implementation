9a6dcd34673c97b413a3bb27e04fb0e94fa66538
theawesomew <theawesomew@gmail.com>
2022-01-31 14:35:09 +1100

    Add files via upload

---
+++ utils/test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "C:/Program Files (x86)/Intel RealSense SDK 2.0/include/librealsense2/rs.hpp"
+
+int main () {
+
+    rs2::pipeline p;
+
+    p.start();
+
+    while (true) {
+        rs2::frameset frames = p.wait_for_frames();
+        rs2::depth_frame depth_frame = frames.get_depth_frame();
+
+        int width = depth_frame.get_width(), height = depth_frame.get_height();
+
+        float distance_to_centre = depth_frame.get_distance(width/2, height/2);
+
+        std::cout << distance_to_centre << std::endl;
+    }
+
+    return 0;
+}
