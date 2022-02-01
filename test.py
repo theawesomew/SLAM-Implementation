874f04b1853b960735f1571f0fd3490bac9bee4b
theawesomew <theawesomew@gmail.com>
2022-02-02 09:59:56 +1100

    Add files via upload

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 from utils.robot2 import *
 import time
+import sys
 from math import *
-
 
 robot = Robot(0,0,90,0.9,0.9)
 
 try:
     robot.setServoPosition(0.5)
-    time.sleep(2)
+    time.sleep(float(sys.argv[1]))
     robot.setServoPosition(0)
 except KeyboardInterrupt:
     robot.setServoPosition(0)
