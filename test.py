d48d36b87c2e6a5fdb17f8443a137ea808836a4f
theawesomew <theawesomew@gmail.com>
2022-02-01 20:36:04 +1100

    Add files via upload

--- test.py
+++ test.py
@@ -6,7 +6,10 @@ from math import *
 robot = Robot(0,0,90)
 
 try:
-    robot.turn(True)
+    robot.leftWheel()
+    time.sleep(1)
+    robot.stop()
+    robot.rightWheel()
     time.sleep(1)
     robot.stop()
 except KeyboardInterrupt:
