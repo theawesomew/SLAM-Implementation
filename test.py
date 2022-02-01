e32d3a90449030b9660acf6f6707f58a7377f6fd
theawesomew <theawesomew@gmail.com>
2022-02-01 19:34:36 +1100

    Add files via upload

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 from utils.robot2 import *
 import time
+from math import *
 
 
 robot = Robot(0,0,90)
 
 try:
-    robot.setServoPosition(0.5)
+    robot.forward(1)
+    time.sleep(1)
+    robot.turn(True)
+    time.sleep(90 * (1/(2*degrees(asin(9/18.25)))))
+    robot.stop()
 except KeyboardInterrupt:
     robot.stop()
