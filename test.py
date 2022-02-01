85e757c43fe01be22deb214f8763b644e3ac3b37
theawesomew <theawesomew@gmail.com>
2022-02-02 09:38:48 +1100

    Add files via upload

--- test.py
+++ test.py
@@ -6,8 +6,9 @@ from math import *
 robot = Robot(0,0,90)
 
 try:
-    robot.leftWheel()
-    time.sleep(1)
-    robot.stop()
+    robot.setServoPosition(0.1)
+    time.sleep(0.1)
+    robot.setServoPosition(0)
 except KeyboardInterrupt:
+    robot.setServoPosition(0)
     robot.stop()
