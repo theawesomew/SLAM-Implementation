c7d198c4f1c7250001c21a24a51ab60abd255ad6
theawesomew <theawesomew@gmail.com>
2022-02-01 19:36:42 +1100

    Add files via upload

--- test.py
+++ test.py
@@ -6,10 +6,7 @@ from math import *
 robot = Robot(0,0,90)
 
 try:
-    robot.forward(1)
-    time.sleep(1)
-    robot.turn(True)
-    time.sleep(90 * (1/(2*degrees(asin(9/18.25)))))
+    robot.follow("FR")
     robot.stop()
 except KeyboardInterrupt:
     robot.stop()
